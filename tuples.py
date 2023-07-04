a994a883b300fbb61f9856e7c5c8ec9a640115a5
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-04 12:01:05 +0200

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,4 +1,4 @@
-# List ["item", "item","item" ]
 # TUPLE ("item", "item","item" )
+# List ["item", "item","item" ]
 # Set {"item", "item","item" }
 # Dictionary {"key":"value", "key":"value", "key":"value"}
