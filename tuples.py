6198b2fb739239b18e6b37a380acf4b248f82337
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-06 11:12:43 +0200

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -83,9 +83,51 @@ else:
 
 
 
-
-
 #-------------------  Update Tuple  -----------------------
+
+#Change Tuple Values
+#Once a tuple is created, you cannot change its values.
+#  Tuples are unchangeable, or immutable as it also is called.
+
+#But there is a workaround. You can convert the tuple into a list,
+#  change the list, and convert the list back into a tuple
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1]="kiwi"
+x=tuple(y)
+
+print(x)
+# ('apple', 'kiwi', 'cherry')
+
+
+# Add Items
+# Since tuples are immutable, they do not have a built-in append() method,
+#  but there are other ways to add items to a tuple.
+
+# 1. Convert into a list: Just like the workaround for changing a tuple,
+#  you can convert it into a list, add your item(s), and convert it back
+#  into a tuple.
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+print(thistuple)
+# ('apple', 'banana', 'cherry', 'orange')
+
+
+# 2. 2. Add tuple to a tuple. You are allowed to add tuples to tuples,
+#  so if you want to add one item, (or many), create a new tuple with
+#  the item(s), and add it to the existing tuple
+
+fruitsTup = ("apple", "banana", "cherry")
+y=("orange", )
+fruitsTup += y
+
+print(fruitsTup)
+# ('apple', 'banana', 'cherry', 'orange')
 
 #-------------------  Unpack Tuple  -----------------------
 
