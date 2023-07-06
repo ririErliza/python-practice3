c4fb3550acc45fbb9dca98a2f375e92bc819279a
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-06 11:30:33 +0200

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,3 +2,11 @@
 # Tuple ("item", "item","item" )
 # SET {"item", "item","item" }
 # Dictionary {"key":"value", "key":"value", "key":"value"}
+
+
+#----------- Access Set Items -----------------------
+#---------------- Add Set Items  --------------------
+#--------------- Remove Set Items  ------------------
+#------------------- Loop Sets  ---------------------
+#------------------- Join Sets  ---------------------
+#----------------- Set Methods  ---------------------
